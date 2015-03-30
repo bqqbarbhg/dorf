74fecce34a3e10a26c24df0590b88feb39b3fd63
bqqbarbhg <samuli.1995@hotmail.com>
2015-03-31 01:23:44 +0300

    Win32: Open window

--- src/main_gl_win32.cpp
+++ src/main_gl_win32.cpp
@@ -1,11 +1,48 @@
 #include <Windows.h>
 
+bool run = true;
+LRESULT CALLBACK WinProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CLOSE:
+		run = false;
+		break;
+	}
+
+	return DefWindowProc(window, message, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdShow)
 {
-	typedef const char*(*get_message_t)();
-	HMODULE module = LoadLibrary("frontend_gl.dll");
-	get_message_t get_message = (get_message_t)GetProcAddress(module, "get_message");
-	MessageBox(NULL, get_message(), "Winapi box", 0);
+	WNDCLASS wnd_class = { 0 };
+	wnd_class.style = CS_OWNDC;
+	wnd_class.lpfnWndProc = WinProc;
+	wnd_class.hInstance = instance;
+	wnd_class.lpszClassName = "main_class";
+	
+	ATOM main_class = RegisterClassA(&wnd_class);
+	if (!main_class) return 1;
+
+	HWND window = CreateWindowExA(
+		0, MAKEINTATOM(main_class),
+		"Test Window",
+		WS_TILEDWINDOW|WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		1280, 720,
+		NULL, NULL, instance, NULL);
+
+	while (run)
+	{
+		MSG message;
+		while (PeekMessageA(&message, window, 0, 0, 1))
+		{
+			TranslateMessage(&message);
+			DispatchMessageA(&message);
+		}
+		Sleep(10);
+	}
+
 	return 0;
 }
 
