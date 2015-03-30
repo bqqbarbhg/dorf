f0494a36cbae67a92477f6904b86072087db359a
bqqbarbhg <samuli.1995@hotmail.com>
2015-03-31 00:18:35 +0300

    Super simple build

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+cd bin
+
+cl -Zi ../src/main_gl_win32.cpp user32.lib
+
---
+++ src/main_gl_win32.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+	puts("Hello world :D");
+	return 0;
+}
+
