0451c3653f53bfb70f71c26c23c4929e05e8b7ac
bqqbarbhg <samuli.1995@hotmail.com>
2015-03-31 01:02:11 +0300

    Cleaned build.bat

--- build.bat
+++ build.bat
@@ -3,5 +3,10 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 cd bin
 
-cl -Zi -SUBSYSTEM:WINDOWS ../src/main_gl_win32.cpp user32.lib
+set IgnoreWarn= -wd4100
+set CLFlags= -MT -nologo -Od -W4 -WX -Zi %IgnoreWarn%
+set LDFlags= -opt:ref user32.lib 
+
+cl %CLFlags% ../src/main_gl_win32.cpp /link %LDFlags% -subsystem:windows 
+cd ..
 
